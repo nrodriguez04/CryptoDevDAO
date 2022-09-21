abb3567d2c981f3213b61e60567ab86eb831e801
nrodriguez04 <nicolasmrodriguez3@gmail.com>
2022-09-21 16:21:23 -0500

    adding folders

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x4d3b85E558C07278Daa0E3aDdf71a13FBB4877C1";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
